0f359a82445784c29cf2ec8e74266d7485e00ec7
Gflex39 <78106045+Gflex39@users.noreply.github.com>
2021-07-15 12:40:11 -0400

    Changes to find Entropy and Enthalpy at the MP

--- Mg-Bi Gibbs.py
+++ Mg-Bi Gibbs.py
@@ -15,96 +15,121 @@ from pycalphad.plot.utils import phase_legend
 import numpy as np
 import alphashape
 
-# Load database and choose the phases that will be plotted
-db_nbre = Database('Mg-Bi.tdb')
-my_phases_nbre = ['LIQUID', 'BI2MG3_H', 'BI2MG3_L']
+def potfinder(temper,plot):
+    # Load database and choose the phases that will be plotted
+    db_nbre = Database('Mg-Bi.tdb')
+    my_phases_nbre = ['LIQUID', 'BI2MG3_H', 'BI2MG3_L']
 
-# Get the colors that map phase names to colors in the legend
-legend_handles, color_dict = phase_legend(my_phases_nbre)
-hullval=[]
-fig = plt.figure(num=1,figsize=(9,6))
-ax = fig.gca()
-temper=1000
-# Loop over phases, calculate the Gibbs energy, and scatter plot GM vs. X(RE)
-phase_potentials={}
-for phase_name in my_phases_nbre:
-    result = calculate(db_nbre, ['BI','MG','VA'], phase_name, P=101325, T=temper, output='GM')
-    
-       
-        #ax.scatter(result.X.sel(component='MG'), result.GM, marker='.', s=5, color=color_dict[phase_name])
-    
-    print(phase_name)
-    temp=result.GM.values
-    tempx=result.X.sel(component='MG').values
-    
-    x = tempx.squeeze()     
-    y = temp.squeeze()
+    # Get the colors that map phase names to colors in the legend
+    legend_handles, color_dict = phase_legend(my_phases_nbre)
+    hullval=[]
+    fig = plt.figure(num=1,figsize=(9,6))
+    ax = fig.gca()
 
-    #Here the (x,y) pairs of points are but into a more simple and callable form
-     #This portion of the code needs to be changed but all it does is get the hull of the gibbs surface
-    if phase_name=='LIQUID':
-        minihull=[]
-    for i in range(len(x)):
-        app=[x[i],y[i]]
-        if phase_name == 'LIQUID':
-            minihull.append(app)
-        hullval.append(app)
-    if phase_name=='LIQUID':
-        minihull=np.array(minihull)
-        liquidx=[]
-        liquidy=[]
-        lhull=ConvexHull(minihull)
-        for simplex in lhull.simplices:
-            liquidx.extend(minihull[simplex,0].tolist())
-            liquidy.extend(minihull[simplex,1].tolist())
-        x=liquidx
-        y=liquidy
-    #From this point all the code makes a polynomial estimate of the specific phase curve 
-    #then find the derivative
-    estimate=np.polyfit(x,y,8)
-    derivative=background.find_derivative(estimate)
-    alphapot=[]
-    betapot=[]
-    for i in range(len(x)):
-        slope=derivative(x[i])
-        potential=background.find_potentials(slope, x[i], y[i], True)
-        alphapot.append(potential[0])
-        betapot.append(potential[1])
-    print(len(x))
-    phase_potentials[phase_name]={'alpha':alphapot,'beta':betapot,'x':x}
-  
+    # Loop over phases, calculate the Gibbs energy, and scatter plot GM vs. X(RE)
+    phase_potentials={}
+    for phase_name in my_phases_nbre:
+        result = calculate(db_nbre, ['BI','MG','VA'], phase_name, P=101325, T=temper, output='GM')
+        
+        
+            #ax.scatter(result.X.sel(component='MG'), result.GM, marker='.', s=5, color=color_dict[phase_name])
+        
+        print(phase_name)
+        temp=result.GM.values
+        tempx=result.X.sel(component='MG').values
+        
+        x = tempx.squeeze()     
+        y = temp.squeeze()
 
-    ax.scatter(x,y, marker='.', s=5, color=color_dict[phase_name])
+        #Here the (x,y) pairs of points are but into a more simple and callable form
+        #This portion of the code needs to be changed but all it does is get the hull of the gibbs surface
+        if phase_name=='LIQUID':
+            minihull=[]
+        for i in range(len(x)):
+            app=[x[i],y[i]]
+            if phase_name == 'LIQUID':
+                minihull.append(app)
+            hullval.append(app)
+        if phase_name=='LIQUID':
+            minihull=np.array(minihull)
+            liquidx=[]
+            liquidy=[]
+            lhull=ConvexHull(minihull)
+            for simplex in lhull.simplices:
+                liquidx.extend(minihull[simplex,0].tolist())
+                liquidy.extend(minihull[simplex,1].tolist())
+            x=liquidx
+            y=liquidy
+        #From this point all the code makes a polynomial estimate of the specific phase curve 
+        #then find the derivative
+        estimate=np.polyfit(x,y,8)
+        derivative=background.find_derivative(estimate)
+        alphapot=[]
+        betapot=[]
+        for i in range(len(x)):
+            slope=derivative(x[i])
+            potential=background.find_potentials(slope, x[i], y[i], True)
+            alphapot.append(potential[0])
+            betapot.append(potential[1])
+        print(len(x))
+        phase_potentials[phase_name]={'alpha':alphapot,'beta':betapot,'x':x}
 
         
-    #This commented portion takes the values attained and turns them into a csv file labeling what it is
-    # background.csvform("Mg", "Bi", phase_name, temper)
-#This finds the convex hull of the plot
-hullval=np.array(hullval)
-hull = ConvexHull(hullval)
+    
+        if plot:
+            ax.scatter(x,y, marker='.', s=5, color=color_dict[phase_name])
 
-# plt.plot(hullval[:][0], hullval[:][1], 'o')
-#for simplex in hull.simplices:
+    if plot:    
+            #This commented portion takes the values attained and turns them into a csv file labeling what it is
+            # background.csvform("Mg", "Bi", phase_name, temper)
+        #This finds the convex hull of the plot
+        hullval=np.array(hullval)
+        hull = ConvexHull(hullval)
 
-    #plt.plot(hullval[simplex,0], hullval[simplex,1], 'k-')
-#plt.plot(hullval[hull.vertices,0], hullval[hull.vertices,1], 'r--', lw=2)
-#plt.plot(hullval[hull.vertices[0],0], hullval[hull.vertices[0],1], 'ro')
+        # plt.plot(hullval[:][0], hullval[:][1], 'o')
+        #for simplex in hull.simplices:
 
-# plt.show()
-# Format the plot
-ax.set_xlabel('X(Mg)')
-ax.set_ylabel('GM')
-ax.set_xlim((0, 1))
-ax.legend(handles=legend_handles, loc='center left', bbox_to_anchor=(1, 0.6))
+            #plt.plot(hullval[simplex,0], hullval[simplex,1], 'k-')
+        #plt.plot(hullval[hull.vertices,0], hullval[hull.vertices,1], 'r--', lw=2)
+        #plt.plot(hullval[hull.vertices[0],0], hullval[hull.vertices[0],1], 'ro')
 
-pplot=plt.figure(2)
-ax2=pplot.gca()
-phase='LIQUID'
-ax2.set_xlabel('Mole Percent of Mg',fontsize=16)
-ax2.set_ylabel('Chemical Potential')
-ax2.set_title('Chemical Potential of the '+phase+' phase at '+str(temper)+" K")
-ax2.scatter(phase_potentials[phase]['x'],phase_potentials[phase]['alpha'],color='red',label='Pure Mg potential')
-ax2.scatter(phase_potentials[phase]['x'],phase_potentials[phase]['beta'],color='blue',label='Pure Bi potential')
-ax2.legend(loc='best', scatterpoints=100)
+        # plt.show()
+        # Format the plot
+        ax.set_xlabel('X(Mg)')
+        ax.set_ylabel('GM')
+        ax.set_xlim((0, 1))
+        ax.legend(handles=legend_handles, loc='center left', bbox_to_anchor=(1, 0.6))
 
-plt.show()
+        pplot=plt.figure(2)
+        ax2=pplot.gca()
+        phase='LIQUID'
+        ax2.set_xlabel('Mole Percent of Mg',fontsize=16)
+        ax2.set_ylabel('Chemical Potential')
+        ax2.set_title('Chemical Potential of the '+phase+' phase at '+str(temper)+" K")
+        ax2.scatter(phase_potentials[phase]['x'],phase_potentials[phase]['alpha'],color='red',label='Pure Mg potential')
+        ax2.scatter(phase_potentials[phase]['x'],phase_potentials[phase]['beta'],color='blue',label='Pure Bi potential')
+        ax2.legend(loc='best', scatterpoints=100)
+
+        plt.show()
+    return phase_potentials
+temps=[]
+pots=[]
+
+for i in range(630,670):
+    temps.append(i)
+    curr=potfinder(i, False)
+    minimum=min(curr['LIQUID']['x'])
+    for j in range(len(curr['LIQUID']['x'])):
+        if curr['LIQUID']['x'][j]==minimum:
+            if i==650:
+                Gibb=curr['LIQUID']['alpha'][j]
+            pots.append(curr['LIQUID']['alpha'][j])
+            break
+potestimate=np.polyfit(temps,pots,4)
+potderivative=background.find_derivative(potestimate)
+entropy=potderivative(650)
+enthalpy=Gibb+entropy*650
+print("Enthalpy="+str(enthalpy))
+print("Entropy="+str(entropy))
+
+
